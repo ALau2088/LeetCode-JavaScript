2b3d46010e85c57fb82526d71b928493a1f269f0
ALau2088 <alau12004@yahoo.com>
2020-02-09 14:17:22 -0800

    complete 46-permuntations recursively

---
+++ 46-Permutations.js
@@ -0,0 +1,71 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+/*
+*IOCE
+I:array of distinct integers
+O: array of permutations
+C: none
+E: empty array, one element
+*EXAMPLES-on paper
+*PSEUDOCODE
+FUNC permute (nums):
+    IF nums.length EQ 0:
+        RETURN nums
+    
+    permutations(p) = []
+    
+    FOR num in nums:
+        remainingNums = arr not including num
+        _permute([num], remainingNums)
+    
+    FUNC _permute(permutation, remainingNums):
+        // BASE CASE - one element remainingNums
+        IF remainingNums.length EQ 1:
+            permutation.push(remainingNums[0])
+            permutations.push(permutation)
+        ELSE:
+            FOR num in remainingNums:
+                permutation+=num
+                remainingNums = arr not including num
+            _permute(permutation, remainingNums)
+        END IF
+    END FUNC
+END FUNC
+*/
+var permute = function(nums) {
+  if (nums.length === 0) {
+    return nums;
+  }
+
+  var p = [];
+  var _permute = function(permutation, nums) {
+    if (nums.length === 1) {
+      permutation.push(nums[0]);
+      p.push(permutation);
+      return;
+    } else {
+      for (var j = 0; j < nums.length; j++) {
+        var newPermutation = permutation.slice();
+        newPermutation.push(nums[j]);
+        var remainingNums = nums.slice();
+        remainingNums.splice(j, 1);
+        _permute(newPermutation, remainingNums);
+      }
+    }
+  };
+
+  // for (var i = 0; i < nums.length; i++) {
+  //   var remainingNums = nums.slice();
+  //   remainingNums.splice(i, 1);
+  //   var permutation = [];
+  //   permutation.push(nums[i]);
+  //   _permute(permutation, remainingNums);
+  // }
+
+  _permute([], nums);
+  return p;
+};
+
+// Time and space complexity is O(n!)
